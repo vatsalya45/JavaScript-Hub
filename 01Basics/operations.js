7fbdd5a1ce779d692ffa663c2290e2c5aa4ec17f
Vatsalya Shukla <vatsalyas108@gmail.com>
2025-04-14 15:19:59 +0000

    Operations

---
+++ 01Basics/operations.js
@@ -0,0 +1,11 @@
+
+let value = 45
+let negetiveValue = -value
+
+console.log(negetiveValue)
+console.log(2**15)
+
+console.log(+true) //1
+console.log(+"")  //0
+//console.log(true+)  -->Error
+//console.log(""+)   --> Error
