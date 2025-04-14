406938f38e9fcb65c26264481ab15cd7e339cf38
Vatsalya Shukla <vatsalyas108@gmail.com>
2025-04-14 15:07:25 +0000

    Conversion

---
+++ 01Basics/datatypes.js
@@ -0,0 +1,15 @@
+//Conversion into boolean
+let name = "vasu"
+let itsBoolean = Boolean(name)
+console.log(itsBoolean)
+//1 or any non 0 number => true 
+// 0 => false
+//"" => false
+//"abd"  => true
+
+//Conversion into String 
+
+let numberA = 99
+let stringA = String(numberA)
+console.log(stringA)
+console.log(typeof stringA)
